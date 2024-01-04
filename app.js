62b96a1bd1ed0c02904d16dad966204d5deb16f2
laurent.lin <laurent.lin@aswatson.com>
2024-01-04 16:42:57 +0800

    提交代码

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// 静态资源中间件，将 public 目录映射到根路径
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 用于处理根路径的路由
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// 启动服务器
+const port = 3344;
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
